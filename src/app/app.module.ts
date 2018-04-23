2e18ea7c266952d2adc97b613abd822f4414aad8
acabezar <uzumaki1995>
2018-04-23 23:54:26 +0200

    final commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 
-import {AppComponent} from './app.component';
-import {ContactComponent} from './pages/contact/contact.component';
-import {WelcomeComponent} from './pages/welcome/welcome.component';
-import {AppModuleRoutes} from './app.module.routes';
-import {StepComponent} from './shared/components/step/step.component';
-import {LayoutPageComponent} from './pages/layouts/layout-page/layout-page.component';
-import {LayoutWelcomeComponent} from './pages/layouts/layout-welcome/layout-welcome.component';
+import { AppComponent } from './app.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { AppModuleRoutes } from './app.module.routes';
+import { StepComponent } from './shared/components/step/step.component';
+import { LayoutPageComponent } from './pages/layouts/layout-page/layout-page.component';
+import { LayoutWelcomeComponent } from './pages/layouts/layout-welcome/layout-welcome.component';
 import { ExamplesComponent } from './pages/examples/examples/examples.component';
 import { ButtonComponent } from './shared/components/button/button.component';
+import { CardModule } from "primeng/components/card/card";
+import { ProgressBarModule } from "primeng/components/progressbar/progressbar";
+import { CarouselModule } from "primeng/components/carousel/carousel";
 
 
 @NgModule({
@@ -26,10 +29,13 @@ import { ButtonComponent } from './shared/components/button/button.component';
   ],
   imports: [
     BrowserModule,
+    CardModule,
+    ProgressBarModule,
+    CarouselModule,
     AppModuleRoutes
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule {
 }
