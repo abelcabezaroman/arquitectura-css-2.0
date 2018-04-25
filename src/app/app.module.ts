af369c084a926ab5a23cbd8d294b43d66feb46ee
acabezar <abel.skyhell.cabeza@gmail.com>
2018-04-25 11:31:19 +0200

    2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-import { ContactComponent } from './pages/contact/contact.component';
-import { WelcomeComponent } from './pages/welcome/welcome.component';
-import { AppModuleRoutes } from './app.module.routes';
-import { StepComponent } from './shared/components/step/step.component';
-import { LayoutPageComponent } from './pages/layouts/layout-page/layout-page.component';
-import { LayoutWelcomeComponent } from './pages/layouts/layout-welcome/layout-welcome.component';
-import { ExamplesComponent } from './pages/examples/examples/examples.component';
-import { ButtonComponent } from './shared/components/button/button.component';
-import { CardModule } from "primeng/components/card/card";
-import { ProgressBarModule } from "primeng/components/progressbar/progressbar";
-import { CarouselModule } from "primeng/components/carousel/carousel";
+import {AppComponent} from './app.component';
+import {ContactComponent} from './pages/contact/contact.component';
+import {WelcomeComponent} from './pages/welcome/welcome.component';
+import {AppModuleRoutes} from './app.module.routes';
+import {LayoutPageComponent} from './pages/layouts/layout-page/layout-page.component';
+import {LayoutWelcomeComponent} from './pages/layouts/layout-welcome/layout-welcome.component';
+import {ExamplesComponent} from './pages/examples/examples/examples.component';
+import {ButtonComponent} from './shared/components/button/button.component';
+import {CardModule} from 'primeng/components/card/card';
+import {ProgressBarModule} from 'primeng/components/progressbar/progressbar';
+import {CarouselModule} from 'primeng/components/carousel/carousel';
 
 
 @NgModule({
@@ -21,7 +20,6 @@ import { CarouselModule } from "primeng/components/carousel/carousel";
     AppComponent,
     ContactComponent,
     WelcomeComponent,
-    StepComponent,
     LayoutPageComponent,
     LayoutWelcomeComponent,
     ExamplesComponent,
--- src/app/pages/examples/examples/examples.component.ts
+++ src/app/pages/examples/examples/examples.component.ts
@@ -14,9 +14,9 @@ export class ExamplesComponent implements OnInit {
   ngOnInit() {
     this.cars = this.cars = [
       {vin: 'r3278r2', year: 2010, brand: 'Audi', color: 'Black'},
-      {vin: 'jhto2g2', year: 2015, brand: 'BMW', color: 'White'},
+      {vin: 'jhto2g2', year: 2015, brand: 'BMW', color: 'Coral'},
       {vin: 'h453w54', year: 2012, brand: 'Honda', color: 'Blue'},
-      {vin: 'g43gwwg', year: 1998, brand: 'Renault', color: 'White'},
+      {vin: 'g43gwwg', year: 1998, brand: 'Renault', color: 'Cadetblue'},
       {vin: 'gf45wg5', year: 2011, brand: 'VW', color: 'Red'},
       {vin: 'bhv5y5w', year: 2015, brand: 'Jaguar', color: 'Blue'},
       {vin: 'ybw5fsd', year: 2012, brand: 'Ford', color: 'Yellow'},
